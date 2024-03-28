fa3b0e9317b974b4731f345b8deb82ce052b8ada
Madeline Bloomberg <64097613+MadelineBloomberg@users.noreply.github.com>
2024-03-28 10:43:53 -0400

    Update FizzBuzzAssignment.py

--- FizzBuzzAssignment.py
+++ FizzBuzzAssignment.py
@@ -1,9 +1,9 @@
 for number in range(1,101):
-    if((number % 3) == 0) and ((number % 5) == 0): 
+    if((number % 3) == 0) and ((number % 5) == 0): #if number is a multiple of 3 and 5 print fizzbuzz
         print("fizzbuzz")
-    elif((number % 3) == 0): 
+    elif((number % 3) == 0): #if number is a multiple of 3, print fizz
         print("fizz")
-    elif((number % 5) == 0): #number is a multiple of 5, print buzz
+    elif((number % 5) == 0): #if number is a multiple of 5, print buzz
         print("buzz")
     else:  #else number is not a multiple of 3 print the number
         print(number)
